7708c52c0ceadc2a8bf8e86e8e19a29c1c0071bb
kuchadze <luka.kuchadze25@gmail.com>
2025-07-17 13:15:07 +0400

    create

---
+++ dumy/data.ts
@@ -0,0 +1,50 @@
+// src/data/dummy.ts
+
+export interface CardData {
+  id: number;
+  title: string;
+  imageUrl: string;
+  link: string;
+}
+
+const allCards: CardData[] = [
+  {
+    id: 1,
+    title: "Five Minutes of Me: A Daily Guided Journal for Mindfulness, Gratitude, and Self-Care",
+    imageUrl: "./images/minute-jornal.png",
+    link: "https://www.amazon.com/dp/B0FGY5JMG3", 
+  },
+  {
+    id: 2,
+    title: "Daily Focus Journal for Adults with ADHD: 60-Day Guided Planner to Prioritize Tasks, Improve Focus, and Track Daily Progress",
+    imageUrl: "../images/ADHD-cover.png",
+    link: "https://www.amazon.com/dp/B0FH9MCX1H",
+  },
+  {
+    id: 3,
+    title: "The Self-Care Journal: Daily Prompts and Reflections for a Healthier, Happier You",
+    imageUrl: "../images/Self-care.png",
+    link: "https://www.amazon.com/dp/B0FHG24WRS",
+  },
+  {
+    id: 4,
+    title: "Manifestation Journal: Daily Manifestation Planner to Attract Abundance, Set Intentions & Create the Life You Desire",
+    imageUrl: "../images/Manifestation.png",
+    link: "https://www.amazon.com/dp/B0FHGXL5YF",
+  },
+  {
+    id: 5,
+    title: "2026 Monthly Planner: 12-Month Calendar with Monthly Goals, To-Do Lists, and Notes – January to December",
+    imageUrl: "../images/2026.png",
+    link: "https://www.amazon.com/dp/B0FHK6FTKV",
+  },
+  {
+    id: 6,
+    title: "Fun & Friendly Coloring Book: A Wholesome Mix of Animals, Fantasy, and Everyday Scenes to Color",
+    imageUrl: "../images/coloring-book-cover.png",
+    link: "https://www.amazon.com/dp/B0FHQTQJY1",
+  },
+  // სხვა ბარათები...
+];
+
+export default allCards;
