61f150ac427c61a56f7cc76024add99abce2c96c
kuchadze <luka.kuchadze25@gmail.com>
2025-07-18 22:41:07 +0400

    add site map

---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://velmia.shop', // შენი საიტის URL
+  generateRobotsTxt: true,        // დაამატებს robots.txt-ს ავტომატურად
+};
