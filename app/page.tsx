ae484d87a377556ae2b94aae00238ff1bb665781
kuchadze <luka.kuchadze25@gmail.com>
2025-07-21 16:42:53 +0400

    refactor head

--- app/page.tsx
+++ app/page.tsx
@@ -2,33 +2,63 @@ import Head from "next/head";
 import styles from "./page.module.css";
 import HomeContainer from "@/components/HomePage/Homepage";
 
-
 export default function HomePage() {
-  
-
   return (
     <>
       <Head>
+        <meta
+          property="og:title"
+          content="Global Landing Page - Innovative Solutions Worldwide"
+        />
         <title>Global Landing Page - Innovative Solutions Worldwide</title>
         <meta
           name="description"
           content="Explore our global platform offering innovative solutions, fast delivery, and premium customer service."
         />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap"
-          rel="stylesheet"
-        />
-        <meta name="p:domain_verify" content="66cea5ae5e9b52c325a007d740799c71"/>
+
         <meta
-          name="keywords"
-          content="global services, innovative solutions, fast delivery, premium customer service, landing page"
+          property="og:description"
+          content="Explore our global platform offering innovative solutions, fast delivery, and premium customer service."
         />
-        <meta name="robots" content="index, follow" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://velmia.shop" />
+        <meta
+          property="og:image"
+          content="https://velmia.shop/path-to-your-image.jpg"
+        />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta
+          name="twitter:title"
+          content="Global Landing Page - Innovative Solutions Worldwide"
+        />
+        <meta
+          name="twitter:description"
+          content="Explore our global platform offering innovative solutions, fast delivery, and premium customer service."
+        />
+        <meta
+          name="twitter:image"
+          content="https://velmia.shop/path-to-your-image.jpg"
+        />
+        <link rel="icon" href="/favicon.ico" />
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "WebPage",
+              name: "Global Landing Page - Innovative Solutions Worldwide",
+              url: "https://velmia.shop",
+              description:
+                "Explore our global platform offering innovative solutions, fast delivery, and premium customer service.",
+              image: "../images/Velmia-horizontal-logo.webp",
+            }),
+          }}
+        />
       </Head>
 
       <main className={styles.container}>
-       <HomeContainer />
+        <HomeContainer />
       </main>
     </>
   );
