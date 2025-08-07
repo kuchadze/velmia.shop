99166964db606ced48cfb50675bf9a1f7abf85ce
kuchadze <luka.kuchadze25@gmail.com>
2025-08-07 14:31:38 +0400

    now layout.tsxs add meta pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,8 @@ import Footer from "@/components/Footer/Footer";
 import Header from "@/components/Header/Header";
 import { Analytics } from "@vercel/analytics/next";
 
+// Import the `Head` component from Next.js
+import Head from "next/head";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,10 +28,44 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
+      {/*
+         The <Head> component is not used here in the latest versions of Next.js App Router.
+         Instead, you add the <head> tags directly into the html file, or in this case, 
+         you use the 'script' component for external scripts.
+      */}
+      <head>
+        {/* Meta Pixel Code here */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '782761747563898');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=782761747563898&ev=PageView&noscript=1"
+            alt="Meta Pixel"
+          />
+        </noscript>
+      </head>
+
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <Header />
         <main>{children}</main>
-          <Analytics />
+        <Analytics />
         <Footer />
       </body>
     </html>
