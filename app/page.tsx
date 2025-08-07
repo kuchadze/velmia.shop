14aed05ce4bd5ef3bc5500056f0e3b14134b9c41
kuchadze <luka.kuchadze25@gmail.com>
2025-08-07 14:25:17 +0400

    add meta pixel 2

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,34 @@ export default function HomePage() {
   return (
     <>
       <Head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '782761747563898');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=782761747563898&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+        {/* End Meta Pixel Code */}
+        
+        {/* არსებული Head ტეგები */}
         <meta
           property="og:title"
           content="Global Landing Page - Innovative Solutions Worldwide"
@@ -15,7 +43,6 @@ export default function HomePage() {
           name="description"
           content="Explore our global platform offering innovative solutions, fast delivery, and premium customer service."
         />
-
         <meta
           property="og:description"
           content="Explore our global platform offering innovative solutions, fast delivery, and premium customer service."
@@ -26,7 +53,6 @@ export default function HomePage() {
           property="og:image"
           content="https://velmia.shop/path-to-your-image.jpg"
         />
-
         <meta name="twitter:card" content="summary_large_image" />
         <meta
           name="twitter:title"
@@ -56,31 +82,6 @@ export default function HomePage() {
             }),
           }}
         />
-
-        {/* აქ ჩასვით Meta Pixel-ის კოდი */}
-        {/* Meta Pixel Code */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              !function(f,b,e,v,n,t,s)
-              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-              n.queue=[];t=b.createElement(e);t.async=!0;
-              t.src=v;s=b.getElementsByTagName(e)[0];
-              s.parentNode.insertBefore(t,s)}(window, document,'script',
-              'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '782761747563898');
-              fbq('track', 'PageView');
-            `,
-          }}
-        />
-        <noscript>
-          <img height="1" width="1" style={{ display: 'none' }}
-            src="https://www.facebook.com/tr?id=782761747563898&ev=PageView&noscript=1"
-          />
-        </noscript>
-        {/* End Meta Pixel Code */}
         
       </Head>
 
