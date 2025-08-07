e7446411e75be36562a2957e5bc7579cda033afa
kuchadze <luka.kuchadze25@gmail.com>
2025-08-07 12:42:54 +0400

    add meta pixel

--- app/page.tsx
+++ app/page.tsx
@@ -41,6 +41,7 @@ export default function HomePage() {
           content="https://velmia.shop/path-to-your-image.jpg"
         />
         <link rel="icon" href="/favicon.ico" />
+        
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
@@ -55,6 +56,32 @@ export default function HomePage() {
             }),
           }}
         />
+
+        {/* აქ ჩასვით Meta Pixel-ის კოდი */}
+        {/* Meta Pixel Code */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '782761747563898');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <img height="1" width="1" style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=782761747563898&ev=PageView&noscript=1"
+          />
+        </noscript>
+        {/* End Meta Pixel Code */}
+        
       </Head>
 
       <main className={styles.container}>
